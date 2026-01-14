1d0ec8e696d616799f97f445b3d6077fce9047f9
sagnikbera <sagnikberaofficial@gmail.com>
2026-01-14 19:08:20 +0530

    fix: gitignore

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,10 +1,14 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-    const value = {
 
+    const [userData , setUserData] = useState(null);
+    const [chatData , setChatData] = useState(null);
+
+    const value = {
+        
     }
 
     return (
