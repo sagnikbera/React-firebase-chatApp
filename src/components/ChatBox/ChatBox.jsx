c8eb03fb75649b6222acc1389c7e7b0295589b30
sagnikbera <sagnikberaofficial@gmail.com>
2026-01-18 00:01:33 +0530

    Chat : send+recieve and image sending

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import assets from '../../assets/assets';
 import './ChatBody.css';
 import { AppContext } from '../../context/AppContext';
@@ -11,17 +11,31 @@ import {
 } from 'firebase/firestore';
 import { db } from '../../config/firebase';
 import { toast } from 'react-toastify';
+import { uploadImageToCloudinary } from './../../utils/cloudinary';
 
 const ChatBox = () => {
   const { userData, chatUser, messages, setMessages, messagesId } =
     useContext(AppContext);
   const [input, setInput] = useState('');
+  // input field ref
+  const inputRef = useRef(null);
+  // chat body ref
+  const scrollRef = useRef();
+
+  //page auto-scrolled latest sms
+  useEffect(() => {
+    if (scrollRef.current) {
+      // scrollHeight value scrollTop a set korle akdom niche chole jabe
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   //load chat
   useEffect(() => {
     if (messagesId) {
       const unSub = onSnapshot(doc(db, 'messages', messagesId), (res) => {
-        setMessages(res.data().messages.reverse());
+        setMessages(res.data().messages);
+        // setMessages(res.data().messages.reverse());
         // console.log('====================================');
         // console.log(res.data().messages.reverse());
         // console.log('====================================');
@@ -74,6 +88,77 @@ const ChatBox = () => {
       toast.error(error.message);
     }
     setInput('');
+    //focus on input field
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
+  // send image
+  const sendImage = async (e) => {
+    try {
+      const fileUrl = await uploadImageToCloudinary(e.target.files[0]);
+
+      if (fileUrl && messagesId) {
+        await updateDoc(doc(db, 'messages', messagesId), {
+          messages: arrayUnion({
+            sId: userData.id,
+            image: fileUrl,
+            createdAt: new Date(),
+          }),
+        });
+
+        const userIDs = [chatUser.rId, userData.id];
+
+        userIDs.forEach(async (id) => {
+          const userChatsRef = doc(db, 'chats', id);
+          const userChatSnapshot = await getDoc(userChatsRef);
+
+          if (userChatSnapshot.exists()) {
+            const userChatData = userChatSnapshot.data();
+            const chatIndex = userChatData.chatData.findIndex(
+              (c) => c.messageId == messagesId
+            );
+
+            userChatData.chatData[chatIndex].lastMessage = 'Image';
+            userChatData.chatData[chatIndex].updatedAt = Date.now();
+
+            if (userChatData.chatData[chatIndex].rId === userData.id) {
+              userChatData.chatData[chatIndex].messageSeen = false;
+            }
+
+            await updateDoc(userChatsRef, {
+              chatData: userChatData.chatData,
+            });
+          }
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  };
+
+  //key control - Enter
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      sendMessage();
+    }
+  };
+
+  //time generation
+  const convertTimestamp = (timestamp) => {
+    if (!timestamp) return '';
+    let date = timestamp.toDate();
+    const hr = date.getHours();
+    const min = date.getMinutes().toString().padStart(2, '0'); // to ensure it's 2 digit (05)
+    if (hr > 12) {
+      return hr - 12 + ':' + min + ' PM';
+    } else if (hr === 0) {
+      return '12:' + min + ' AM';
+    } else {
+      return hr + ':' + min + ' AM';
+    }
   };
 
   return chatUser ? (
@@ -105,51 +190,33 @@ const ChatBox = () => {
 
       {/* ================= BODY (SCROLL AREA) ================= */}
       {/* pb-24 keeps space for input */}
-      <div className="flex-1 min-h-0 overflow-y-auto px-4 py-3 pb-24">
-        <div className="chat-msg flex flex-col gap-4">
-          {/* TEXT MESSAGE */}
-          <div className="r-msg">
-            <p className="msg">Hey 👋 This is a text message</p>
-            <div className="msg-info">
-              <img src={assets.profile_img} alt="" />
-              <p>2:30 PM</p>
-            </div>
-          </div>
+      <div className="flex-1 min-h-0 overflow-y-auto px-4 py-3" ref={scrollRef}>
+        <div className="chat-msg gap-4">
+          {messages.map((msg, index) => (
+            // TEXT MESSAGE
+            <div
+              className={msg.sId === userData.id ? 's-msg' : 'r-msg'}
+              key={index}
+            >
+              {/* text sms or Image  */}
+              {msg['image'] ? (
+                <img src={msg.image} alt="" className="msg-img" />
+              ) : (
+                <p className="msg">{msg.text}</p>
+              )}
 
-          {/* IMAGE MESSAGE */}
-          <div className="s-msg">
-            <img src={assets.pic1} alt="sent-img" className="msg-img" />
-            <div className="msg-info">
-              <img src={assets.profile_img} alt="" />
-              <p>2:31 PM</p>
-            </div>
-          </div>
-
-          {/* TEXT MESSAGE */}
-          <div className="s-msg">
-            <p className="msg">Image ta properly fit hocche to? 😄</p>
-            <div className="msg-info">
-              <img src={assets.profile_img} alt="" />
-              <p>2:32 PM</p>
-            </div>
-          </div>
-
-          {/* IMAGE MESSAGE */}
-          <div className="r-msg">
-            <img src={assets.pic1} alt="recv-img" className="msg-img" />
-            <div className="msg-info">
-              <img src={assets.profile_img} alt="" />
-              <p>2:33 PM</p>
-            </div>
-          </div>
-
-          {/* MORE DUMMY TEXT */}
-          {[...Array(10)].map((_, i) => (
-            <div key={i} className={i % 2 ? 's-msg' : 'r-msg'}>
-              <p className="msg">Dummy message {i + 1}</p>
+              {/* avatar */}
               <div className="msg-info">
-                <img src={assets.profile_img} alt="" />
-                <p>2:{34 + i} PM</p>
+                <img
+                  src={
+                    msg.sId === userData.id
+                      ? userData.avatar
+                      : chatUser.userData.avatar
+                  }
+                  alt=""
+                />
+                {/* time  */}
+                <p>{convertTimestamp(msg.createdAt)}</p>
               </div>
             </div>
           ))}
@@ -160,14 +227,27 @@ const ChatBox = () => {
       <div className="shrink-0 px-3 pb-3">
         <div className="flex bg-[#053448] p-3 rounded-2xl items-center">
           <input
+            ref={inputRef}
+            onKeyDown={handleKeyDown}
             className="flex-1 bg-transparent outline-none text-white placeholder-white/30"
             placeholder="Send a message"
             onChange={(e) => setInput(e.target.value)}
             value={input}
+            autoFocus
           />
-
-          <label className="bg-gray-200 p-1 rounded-full cursor-pointer">
-            <img src={assets.gallery_icon} className="w-6 opacity-60" alt="" />
+          {/* img input  */}
+          <input
+            onChange={sendImage}
+            type="file"
+            id="image"
+            accept="image/png , image/jpeg"
+            hidden
+          />
+          <label
+            htmlFor="image"
+            className="bg-gray-200 p-1 rounded-full cursor-pointer"
+          >
+            <img src={assets.gallery_icon} className="w-6" alt="gallery" />
           </label>
 
           <img
